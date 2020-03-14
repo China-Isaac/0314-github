e337655915fbe7a39b654cef60c3d2ebed6c946a
isaac <2390080153@qq.com>
2020-03-14 22:18:45 +0800

    第二次修改

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Test1 test1=new Test1();
+        test1.name="3";
+        System.out.println(test1.name);
     }
 
     // 第一次修改！
